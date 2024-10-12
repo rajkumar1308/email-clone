6c87468b669b645cac4f66c318aa4cd1e82ab992
rajkumar1308 <103402077+rajkumar1308@users.noreply.github.com>
2024-10-12 22:56:21 +0530

    Update UserData.jsx

--- src/components/UserData.jsx
+++ src/components/UserData.jsx
@@ -4,18 +4,27 @@ const UserData = ({ emails, onEmailClick }) => {
         {emails.map((email) => {
           const { id, from, date, subject, short_description, read, favorite } = email;
           return (
+
             <div
               key={id}
               className={`email-item ${read ? 'read' : 'unread'}`}
               onClick={() => onEmailClick(id)}
             >
+
+             
+             <div className='left-head'>
+    <div className='circle'>F</div>
+          </div>
+          <div className="subject-body"> 
               <p className="subject">From: {from.email}</p>
               <p className="subject">Subject: {subject}</p>
               <p className="short-description">{short_description}</p>
               <p className="date">
                 {new Date(date).toLocaleString()} <span>{favorite ? '★' : '☆'}</span>
               </p>
+              </div>
             </div>
+
           );
         })}
       </>
